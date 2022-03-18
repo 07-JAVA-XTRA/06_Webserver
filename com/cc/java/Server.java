72dd14dfca7ae7a0bdd8a481316843ad036cbde1
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-03-18 14:24:53 +0100

    Add files via upload

---
+++ com/cc/java/Server.java
@@ -0,0 +1,21 @@
+
+package com.cc.java;
+
+
+
+class Server {
+
+  public static void main(String[] args) 
+  {
+  
+   
+ 
+  }
+
+  private static void output(String outStr) {
+    System.out.println(outStr);
+  }
+
+
+
+} // EoC
