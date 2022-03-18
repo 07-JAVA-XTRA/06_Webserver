a1c76c0f193c42b09b31b33bf93f551a14dc6f1f
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-03-18 14:21:08 +0000

    1. serverSocket

--- com/cc/java/Server.java
+++ com/cc/java/Server.java
@@ -1,14 +1,27 @@
 
 package com.cc.java;
 
-
+import java.io.IOException;
+import java.net.ServerSocket;
 
 class Server {
 
   public static void main(String[] args) 
   {
   
+   try (ServerSocket serverSocket = new ServerSocket(8080)) {
    
+    output("Hi from " + serverSocket + " \nI'm ready to serve your request!");
+
+
+
+
+
+
+  } catch (IOException e) {
+    e.printStackTrace();
+  }
+
  
   }
 
