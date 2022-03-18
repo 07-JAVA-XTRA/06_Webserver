84b2b7db10054da56695013e6a07a3ed02acf37a
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-03-18 15:07:27 +0000

    Bytes -->Chars-->Strings // Request

--- com/cc/java/Server.java
+++ com/cc/java/Server.java
@@ -1,8 +1,11 @@
 
 package com.cc.java;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 class Server {
 
@@ -13,10 +16,35 @@ class Server {
    
     output("Hi from " + serverSocket + " \nI'm ready to serve your request!");
 
+    while (true) { 
+    // keeps the server alive!
+
+        try(Socket client = serverSocket.accept()){
+          output("msg: " + client.toString());
+          
+          // request als Byte-Steam
+          InputStreamReader inputStream = new InputStreamReader(client.getInputStream());
+          // Bytes --> Characters
+          BufferedReader bfReader = new BufferedReader(inputStream);
+          // Chars in Puffer
+          StringBuilder request = new StringBuilder();
+          // Puffer --> String
+          String tmpLine = bfReader.readLine(); // one Line
+          while (!tmpLine.isBlank()) {  // Leerzeile als Trenner
+            request.append(tmpLine + "\r\n");
+            tmpLine = bfReader.readLine(); // zeile anhängen
+          }
+
+          // Request als String ...
+          output("--> REQUEST: ");
+          output("--> " + request);
 
 
 
 
+
+        }
+    }
 
   } catch (IOException e) {
     e.printStackTrace();
